960da7c23d03aac8f1033c1a4cd475fb281492d9
Great <jgreat7702gmail.com>
2024-10-11 00:24:29 +0100

    task0

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+thew python function module
+"""
+
+
 def canUnlockAll(boxes):
     """
     the can unlock function checks if
@@ -7,12 +12,12 @@ def canUnlockAll(boxes):
     unlocked = [False] * len(boxes)  # Track unlocked boxes
     unlocked[0] = True  # First box is unlocked by default
     keys = [0]  # Start with the first box's keys
-    
+
     while keys:
         current_key = keys.pop()
         for key in boxes[current_key]:
             if 0 <= key < len(boxes) and not unlocked[key]:
                 unlocked[key] = True
                 keys.append(key)
-    
+
     return all(unlocked)
